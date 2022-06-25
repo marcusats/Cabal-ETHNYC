36f6be7fb23a0874dc36b777b8715bf68427bcfc
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2022-06-25 19:46:57 -0400

    rainbow

--- cabal-client/helper/connectWallet.js
+++ cabal-client/helper/connectWallet.js
@@ -1,42 +1,25 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { ethers } from "ethers";
-import Web3 from "web3";
-import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
-const INFURA_ID =
-	"https://mainnet.infura.io/v3/8f36546336664817bc3db0bd4240e3bc";
+import { ethers, providers} from "ethers"; 
+import {useEffect} from 'react'
 
 const provider = new WalletConnectProvider({
-	infuraId: INFURA_ID,
+	rpc: {
+	  1: "https://mainnet.infura.io/v3/8f36546336664817bc3db0bd4240e3bc",
+	},
 });
 
+
 export const connectWalletConnect = async () => {
-	const ethersProvider = new ethers.providers.Web3Provider(provider);
-	await provider.enable();
-	console.log({ ethersProvider });
-	console.log({ provider });
-
-	await provider.qrcodeModal.open();
-
-	// const uri = provider.uri;
-	// // display QR Code modal
-	// WalletConnectQRCodeModal.open(
-	// 	uri,
-	// 	() => {
-	// 		console.log("QR Code Modal closed");
-	// 	},
-	// 	true // isNode = true
-	// );
-	if (!provider.connected) {
-		// create new session
+	
+	try {
+		await provider.enable();
+		const ethersProvider = new providers.Web3Provider(provider);
+		console.log({ ethersProvider });
+		console.log({ provider });
+	}catch (error){
+		console.log(error)
 	}
-	provider.on("accountsChanged", (accounts) => {
-		// you can access the accounts here
-		console.log({ accounts });
-	});
-	provider.on("disconnect", (code, reason) => {
-		//fired when disconnecting WalletConnect
-		console.log(code, reason);
-	});
+	
 };
 export const connectMetamask = async () => {
 	try {
--- cabal-client/pages/_app.js
+++ cabal-client/pages/_app.js
@@ -6,6 +6,44 @@ import Header from "../components/header";
 import { Context, Provider } from "../providers/provider";
 import { Toaster } from "react-hot-toast";
 import { connectMetamask, connectWalletConnect } from "../helper/connectWallet";
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import  {ethers} from "ethers";
+
+const { chains, provider } = configureChains(
+	[chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+	[
+	  alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+	  publicProvider()
+	]
+  );
+  
+  const { connectors } = getDefaultWallets({
+	appName: 'My RainbowKit App',
+	chains
+  });
+  
+  const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider
+})
+
+
+
 function Root({ Component, pageProps }) {
 	const toast = useToast();
 	const { walletId, setWalletId, setLoggedIn, loggedIn } = useContext(Context);
@@ -45,23 +83,27 @@ function Root({ Component, pageProps }) {
 }
 function App({ Component, pageProps }) {
 	return (
-		<Provider>
-			<Toaster
-				position="top-center"
-				reverseOrder={false}
-				gutter={8}
-				containerClassName=""
-				containerStyle={{}}
-				toastOptions={{
-					className: "toaster",
-					duration: 5000,
-					style: {},
-				}}
-			/>
-			<ChakraProvider theme={chakraTheme}>
-				<Root Component={Component} pageProps={pageProps} />
-			</ChakraProvider>
-		</Provider>
+	<WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+			<Provider>
+				<Toaster
+					position="top-center"
+					reverseOrder={false}
+					gutter={8}
+					containerClassName=""
+					containerStyle={{}}
+					toastOptions={{
+						className: "toaster",
+						duration: 5000,
+						style: {},
+					}}
+				/>
+				<ChakraProvider theme={chakraTheme}>
+					<Root Component={Component} pageProps={pageProps} />
+				</ChakraProvider>
+			</Provider>
+		</RainbowKitProvider>
+    </WagmiConfig>
 	);
 }
 
