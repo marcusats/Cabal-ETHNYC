03ec44cceffaf8be90a4e437b60b5a6d6b395c7b
Adam Farid <afarid@stevens.edu>
2022-06-25 19:21:44 -0400

    added tables

--- cabal-client/pages/manageAccess/index.js
+++ cabal-client/pages/manageAccess/index.js
@@ -3,40 +3,41 @@ import { useRouter } from "next/router";
 import { Button } from "@chakra-ui/react";
 
 export default function ManageAccess() {
-	const router = useRouter();
-	const services = [
-		{ name: "Facebook", id: "facebook" },
-		{ name: "Github", id: "facebook" },
-		{ name: "Quae", id: "facebook" },
-		{ name: "Gypsy", id: "facebook" },
-	];
-	return (
-		<div className="manage-access">
-			<h2 className="Title">Manage Access</h2>
-			{/* <button
+  const router = useRouter();
+  const services = [
+    { name: "Facebook", id: "facebook" },
+    { name: "Github", id: "github" },
+    { name: "Quae", id: "quae" },
+    { name: "Gypsy", id: "gypsy" },
+  ];
+  return (
+    <div className="manage-access">
+      <h2 className="Title">Manage Access</h2>
+      {/* <button
 				onClick={() => {
 					router.push("/manageAccess/glah");
 				}}>
 				<p>go to "glah"</p>
 			</button> */}
-			<div className="services-container">
-				{services.map((item) => {
-					return (
-						<div className="inlineRow">
-							<h3>{item.name}</h3>
-							<span>
-								<Button
-									onClick={() => {
-										router.push(`/manageAccess/${item.id}`);
-									}}>
-									Details
-								</Button>
-								<Button variant="danger">Revoke</Button>
-							</span>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+      <div className="services-container">
+        {services.map((item) => {
+          return (
+            <div className="inlineRow">
+              <h3>{item.name}</h3>
+              <span>
+                <Button
+                  onClick={() => {
+                    router.push(`/manageAccess/${item.id}`);
+                  }}
+                >
+                  Details
+                </Button>
+                <Button variant="danger">Revoke</Button>
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
