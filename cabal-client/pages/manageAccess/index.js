d471746deee4064f6da50cb4c9f81af1d0f4a122
Adam Farid <afarid@stevens.edu>
2022-06-25 15:57:27 -0400

    Manage access page added

--- cabal-client/pages/manageAccess/index.js
+++ cabal-client/pages/manageAccess/index.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { Button } from "@chakra-ui/react";
 
 export default function ManageAccess() {
-	const router = useRouter();
-
-	return (
-		<div>
-			<h2>Manage Access</h2>
-			<button
+  const router = useRouter();
+  const services = [
+    { name: "Facebook", id: "facebook" },
+    { name: "Github", id: "facebook" },
+    { name: "Quae", id: "facebook" },
+    { name: "Gypsy", id: "facebook" },
+  ];
+  return (
+    <div className="manage-access">
+      <h2 className="Title">Manage Access</h2>
+      {/* <button
 				onClick={() => {
 					router.push("/manageAccess/glah");
 				}}>
 				<p>go to "glah"</p>
-			</button>
-		</div>
-	);
+			</button> */}
+      <div className="services-container">
+        {services.map((item) => {
+          return (
+            <div className="inlineRow">
+              <h3>{item.name}</h3>
+              <span>
+                <Button>Details</Button>
+                <Button variant="danger">Revoke</Button>
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
