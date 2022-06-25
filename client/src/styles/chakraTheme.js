10ff8901a4576d277ae050daef3d2ebf3cd32be3
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-06-25 19:34:09 -0400

    moved things to demo client

---
+++ client/src/styles/chakraTheme.js
@@ -0,0 +1,146 @@
+import { extendTheme } from "@chakra-ui/react";
+const config = {
+	initialColorMode: "light",
+	useSystemColorMode: false,
+};
+const colors = {
+	primary: "#3da9fc",
+	primaryLight: "#094067",
+	primaryDark: "#83ccf4",
+	text: "#021929",
+	textSecondary: "#192934",
+	background: "#fcfcfc",
+	foreground: "#e9e9e9",
+	accent: "#ef4565",
+	danger: "#F66363",
+};
+const components = {
+	Button: {
+		baseStyle: {
+			margin: 0.5,
+			_hover: { opacity: "75%" },
+			_focus: {
+				boxShadow: "none",
+			},
+		},
+		variants: {
+			contained: {
+				color: "white",
+				backgroundColor: colors.primary,
+			},
+			danger: {
+				color: "white",
+				backgroundColor: colors.accent,
+			},
+		},
+		outlined: {
+			borderWidth: 3,
+			borderStyle: "solid",
+			borderColor: colors.text,
+			color: colors.text,
+			_hover: { backgroundColor: colors.foreground },
+		},
+		text: {
+			_hover: { backgroundColor: colors.foreground },
+		},
+		link: {
+			color: colors.link,
+			_hover: { backgroundColor: colors.foreground },
+		},
+		filled: {
+			backgroundColor: colors.foreground,
+		},
+		defaultProps: {
+			variant: "contained",
+		},
+	},
+
+	IconButton: {
+		variants: {
+			text: {
+				backgroundColor: "transparent",
+			},
+		},
+	},
+	Tooltip: {
+		baseStyle: {
+			borderRadius: 5,
+			backgroundColor: colors.foreground,
+			color: colors.text,
+		},
+	},
+	Alert: {
+		backgroundColor: "red",
+	},
+	Input: {
+		baseStyle: {
+			backgroundColor: colors.primary,
+			borderWidth: 5,
+			borderColor: colors.text,
+			borderStyle: "solid",
+		},
+		// variants: {
+		// 	filled: {},
+		// 	outline: {
+		// 		borderColor: colors.textSecondary,
+		// 		borderWidth: 1,
+		// 		borderStyle: "solid",
+		// 	},
+		// },
+		// defaultProps: {
+		// 	variant: "outline",
+		// },
+	},
+	Popover: {
+		variants: {
+			primary: {
+				backgroundColor: colors.foreground,
+			},
+		},
+	},
+	Tag: {
+		variants: {
+			contained: {
+				color: "white",
+				backgroundColor: colors.primary,
+			},
+			danger: {
+				color: "white",
+
+				backgroundColor: colors.danger,
+			},
+			outlined: {
+				borderWidth: 1,
+				borderStyle: "solid",
+				borderColor: colors.secondaryText,
+				color: colors.text,
+				_hover: { backgroundColor: colors.foreground },
+			},
+			text: {
+				_hover: { backgroundColor: colors.foreground },
+			},
+			filled: {
+				backgroundColor: colors.foreground,
+				_hover: {
+					opacity: 0.75,
+				},
+			},
+		},
+	},
+	Drawer: {
+		parts: ["dialog", "header", "body"],
+		variants: {
+			primary: {
+				dialog: {
+					backgroundColor: colors.foreground,
+				},
+			},
+		},
+		defaultProps: {
+			variant: "contained",
+		},
+	},
+};
+
+const chakraTheme = extendTheme({ components, colors, config });
+export default chakraTheme;
