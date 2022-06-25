5052af56a182c3581278a40c8a87a831ba2c79bf
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-06-24 22:40:02 -0400

    chakra theme

--- client/src/styles/chakraTheme.js
+++ client/src/styles/chakraTheme.js
@@ -12,10 +12,11 @@ const colors = {
 	background: "#fcfcfc",
 	foreground: "#e9e9e9",
 	accent: "#ef4565",
+	danger: "#F66363",
 };
 const components = {
 	Button: {
-		basStyle: {
+		baseStyle: {
 			margin: 0.5,
 			_hover: { opacity: "75%" },
 			_focus: {
@@ -23,14 +24,103 @@ const components = {
 			},
 		},
 		variants: {
-			contained: () => ({
+			contained: {
 				color: "white",
 				backgroundColor: colors.primary,
-			}),
-			danger: () => ({
+			},
+			danger: {
 				color: "white",
 				backgroundColor: colors.accent,
-			}),
+			},
+		},
+		outlined: {
+			borderWidth: 1,
+			borderStyle: "solid",
+			borderColor: colors.secondaryText,
+			color: colors.text,
+			_hover: { backgroundColor: colors.foreground },
+		},
+		text: {
+			_hover: { backgroundColor: colors.foreground },
+		},
+		link: {
+			color: colors.link,
+			_hover: { backgroundColor: colors.foreground },
+		},
+		filled: {
+			backgroundColor: colors.foreground,
+		},
+	},
+
+	IconButton: {
+		variant: {
+			text: {
+				backgroundColor: "transparent",
+			},
+		},
+	},
+	Tooltip: {
+		baseStyle: {
+			borderRadius: 5,
+			backgroundColor: colors.foreground,
+			color: colors.text,
+		},
+	},
+	Alert: {
+		backgroundColor: "red",
+	},
+	Input: {
+		baseStyle: {},
+		variants: {
+			filled: {
+				backgroundColor: colors.dark.card,
+			},
+		},
+	},
+	Popover: {
+		variants: {
+			primary: {
+				backgroundColor: colors.foreground,
+			},
+		},
+	},
+	Tag: {
+		variants: {
+			contained: {
+				color: "white",
+				backgroundColor: colors.primary,
+			},
+			danger: {
+				color: "white",
+
+				backgroundColor: colors.danger,
+			},
+			outlined: {
+				borderWidth: 1,
+				borderStyle: "solid",
+				borderColor: colors.secondaryText,
+				color: colors.text,
+				_hover: { backgroundColor: colors.foreground },
+			},
+			text: {
+				_hover: { backgroundColor: colors.foreground },
+			},
+			filled: {
+				backgroundColor: colors.foreground,
+				_hover: {
+					opacity: 0.75,
+				},
+			},
+		},
+	},
+	Drawer: {
+		parts: ["dialog", "header", "body"],
+		variants: {
+			primary: {
+				dialog: {
+					backgroundColor: colors.dark.foreground,
+				},
+			},
 		},
 	},
 };
