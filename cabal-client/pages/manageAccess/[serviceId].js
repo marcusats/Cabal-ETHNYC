da4b821f017407bd65ea59154f5047e9b1c3ef7d
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-06-25 16:35:48 -0400

    started table for manage services details

--- cabal-client/pages/manageAccess/[serviceId].js
+++ cabal-client/pages/manageAccess/[serviceId].js
@@ -1,13 +1,21 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { usMemo } from "react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 export default function ServiceId() {
-  const router = useRouter();
-  const { serviceId } = router.query;
-  return (
-    <div>
-      <h2>{serviceId}</h2>
-      <div></div>
-    </div>
-  );
+	const router = useRouter();
+	const { serviceId } = router.query;
+
+	const columns = usMemo(() => [
+		{ Header: `${serviceId} needs` },
+		{ Header: "For" },
+	]);
+	function Table() {
+		return <></>;
+	}
+	return (
+		<div>
+			<h2>{serviceId}</h2>
+			<Table />
+		</div>
+	);
 }
--- cabal-client/pages/manageAccess/index.js
+++ cabal-client/pages/manageAccess/index.js
@@ -3,35 +3,40 @@ import { useRouter } from "next/router";
 import { Button } from "@chakra-ui/react";
 
 export default function ManageAccess() {
-  const router = useRouter();
-  const services = [
-    { name: "Facebook", id: "facebook" },
-    { name: "Github", id: "facebook" },
-    { name: "Quae", id: "facebook" },
-    { name: "Gypsy", id: "facebook" },
-  ];
-  return (
-    <div className="manage-access">
-      <h2 className="Title">Manage Access</h2>
-      {/* <button
+	const router = useRouter();
+	const services = [
+		{ name: "Facebook", id: "facebook" },
+		{ name: "Github", id: "facebook" },
+		{ name: "Quae", id: "facebook" },
+		{ name: "Gypsy", id: "facebook" },
+	];
+	return (
+		<div className="manage-access">
+			<h2 className="Title">Manage Access</h2>
+			{/* <button
 				onClick={() => {
 					router.push("/manageAccess/glah");
 				}}>
 				<p>go to "glah"</p>
 			</button> */}
-      <div className="services-container">
-        {services.map((item) => {
-          return (
-            <div className="inlineRow">
-              <h3>{item.name}</h3>
-              <span>
-                <Button>Details</Button>
-                <Button variant="danger">Revoke</Button>
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+			<div className="services-container">
+				{services.map((item) => {
+					return (
+						<div className="inlineRow">
+							<h3>{item.name}</h3>
+							<span>
+								<Button
+									onClick={() => {
+										router.push(`/manageAccess/${item.id}`);
+									}}>
+									Details
+								</Button>
+								<Button variant="danger">Revoke</Button>
+							</span>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
