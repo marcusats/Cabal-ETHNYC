b25fa55dd6f0954a9b5eff946f40d227ed477b15
Adam Farid <afarid@stevens.edu>
2022-06-25 20:41:17 -0400

    connected docs

--- cabal-client/components/header.js
+++ cabal-client/components/header.js
@@ -5,24 +5,27 @@ import Logo from "../assets/img/logo.png";
 import WalletConnect from "./walletConnect";
 import { Button } from "@chakra-ui/react";
 export default function Header() {
-	const router = useRouter();
-	return (
-		<header>
-			<button className="content" onClick={() => router.push("/")}>
-				<Image name={"Logo"} src={Logo} width="30px" height="30px" alt="Logo" />
-				<h4>Cabal</h4>
-			</button>
-			<span className="links">
-				<Button variant="text">Documentation</Button>
-				<WalletConnect text={"Connect Wallet"} />
-				{/* <Button variant="contained">Connect Wallet</Button> */}
-				{/* <Link to="/">
+  const router = useRouter();
+  return (
+    <header>
+      <button className="content" onClick={() => router.push("/")}>
+        <Image name={"Logo"} src={Logo} width="30px" height="30px" alt="Logo" />
+        <h4>Cabal</h4>
+      </button>
+      <span className="links">
+        <a href="https://adamfarid453.github.io/CabalDocs/">
+          <Button variant="text">Documentation</Button>
+        </a>
+
+        <WalletConnect text={"Connect Wallet"} />
+        {/* <Button variant="contained">Connect Wallet</Button> */}
+        {/* <Link to="/">
 					<h6>Home</h6>
 				</Link>
 				<Link to="/profile">
 					<h6>Profile</h6>
 				</Link> */}
-			</span>
-		</header>
-	);
+      </span>
+    </header>
+  );
 }
