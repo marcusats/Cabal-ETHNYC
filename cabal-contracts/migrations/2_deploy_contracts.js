5f46ef5acc170675de508c3b524c7995930a3ebd
sam-schmitt <samuelgschmitt@gmail.com>
2022-06-26 02:41:42 -0400

    sdk

--- cabal-contracts/migrations/2_deploy_contracts.js
+++ cabal-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const Oath = artifacts.require("../contracts/Oath.sol");
-//const DataTypeW = artifacts.require("../contracts/DataTypeW.sol");
+const DataTypeW = artifacts.require("../contracts/DataTypeW.sol");
 
 module.exports = function (deployer) {
 	deployer.deploy(Oath).then(function () {
-		return deployer.deploy(DataTypeW, Factory.address);
+		return deployer.deploy(DataTypeW, Oath.address);
 	});
 };
