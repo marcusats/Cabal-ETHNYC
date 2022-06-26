67c8d6af9ef4184de147a608658e0c4b3b08df77
Adam Farid <afarid@stevens.edu>
2022-06-26 01:16:36 -0400

    dashboard changes

--- cabal-client/components/profileInfoButton.js
+++ cabal-client/components/profileInfoButton.js
@@ -1,41 +1,51 @@
 import React, { useState } from "react";
 
 import {
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalFooter,
-	ModalBody,
-	ModalCloseButton,
-	Tooltip,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Tooltip,
 } from "@chakra-ui/react";
-
+import { Input } from "@chakra-ui/react";
 export default function ProfileInfoButton({ item, idx }) {
-	const [visible, setVisible] = useState(false);
-	return (
-		<>
-			<Tooltip
-				label={
-					item.value !== null ? "Completed" : "Todo: Click To Store Your Info"
-				}>
-				<button
-					onClick={() => setVisible(true)}
-					className={`profile-info-btn ${idx % 2 ? "" : "odd"}`}>
-					<span
-						class={`status ${item.value !== null ? "success" : "todo"}`}></span>
-					<p>{item.title}</p>
-				</button>
-			</Tooltip>
+  const [visible, setVisible] = useState(false);
+  function testInput(x) {
+    console.log(x);
+  }
+  return (
+    <>
+      <Tooltip
+        label={
+          item.value !== null ? "Completed" : "Todo: Click To Store Your Info"
+        }
+      >
+        <button
+          onClick={() => setVisible(true)}
+          className={`profile-info-btn ${idx % 2 ? "" : "odd"}`}
+        >
+          <span
+            class={`status ${item.value !== null ? "success" : "todo"}`}
+          ></span>
+          <p>{item.title}</p>
+        </button>
+      </Tooltip>
 
-			<Modal isOpen={visible} size={"lg"} onClose={() => setVisible(false)}>
-				<ModalOverlay />
-				<ModalContent className={"custom-modal"}>
-					<ModalHeader>{item.title}</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>{item.info}</ModalBody>
-				</ModalContent>
-			</Modal>
-		</>
-	);
+      <Modal isOpen={visible} size={"lg"} onClose={() => setVisible(false)}>
+        <ModalOverlay />
+        <ModalContent className={"custom-modal"}>
+          <ModalHeader>{item.title}</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            {console.log(item.vale, typeof item.value)}
+            {typeof item.value === null ? item.value : "error"}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
