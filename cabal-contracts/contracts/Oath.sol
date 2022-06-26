9569b1ae20481bc478fdc4c99477cdae5d7502be
sam-schmitt <samuelgschmitt@gmail.com>
2022-06-26 05:38:00 -0400

    oath up to date

--- cabal-contracts/contracts/Oath.sol
+++ cabal-contracts/contracts/Oath.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Cabal
 pragma solidity ^0.8.0;
 
+
 contract Oath {
     address admin;
     mapping(address => mapping(address => mapping(address => bool))) connections;
@@ -21,11 +22,13 @@ contract Oath {
         connections[msg.sender][service_wallet][data_type_address] = false;
     }
 
-    function checkConnection(address user_wallet, address data_type_address)
+    function checkConnection(address user_wallet, address data_type_address, address requester_address)
         public
         view
         returns (bool)
     {
-        return (connections[user_wallet][msg.sender][data_type_address]);
+        return (connections[user_wallet][requester_address][data_type_address]);
     }
+
+    
 }
