1d1aeadbbcede33babc9b3d4ca3d2e655d9243b3
Bryan Kyritz <bryan@securemeeting.org>
2022-06-26 04:07:06 -0400

    add sdk

--- cabal-js-sdk/src/helpers/ipfsCall.ts
+++ cabal-js-sdk/src/helpers/ipfsCall.ts
@@ -1,32 +1,14 @@
-import { create} from "ipfs-http-client";
+import { CabalClient } from "../index";
 
+export async function pushData(data) {
+  return await CabalClient.ipfs.add(data);
+}
 
-
-const auth =
-'Basic '  + Buffer.from(process.env.PROJECT_ID + ':' + process.env.PROJECT_SECRET).toString('base64');
-
-const client = create({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https',
-    headers: {
-        authorization: auth,
-    },
-});
-
-
-export default function ManageData() {
-
-    function pushData(data) {
-       return (client.add(data))
-    }
-
-    function getData(cid) {
-        let res;
-        const b = ipfs.cat(cid);
-        for await (const chunk of b) {
-         res = new TextDecoder().decode(chunk);
-        }
-        return res
-    }
+export async function getData(cid) {
+  let res;
+  const b = CabalClient.ipfs.cat(cid);
+  for await (const chunk of b) {
+    res = new TextDecoder().decode(chunk);
+  }
+  return res;
 }
